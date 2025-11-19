fd372d6edd9f4c241afa1a6bcd51c436f2bf3efd
DilshanGAM <malithdilshan27@gmail.com>
2025-11-19 22:08:50 +0530

    Day 22 ✏️

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,29 +1,14 @@
-import { useState } from "react";
-import uploadFile from "../utils/mediaUpload";
+
 
 
 
 export default function TestPage() {
 
-	const [file, setFile] = useState(null);
-
-	async function handleUpload(){
-		
-		const url = await uploadFile(file)
-		console.log(url);
-	}
 
 	return (
 		
-			<div className="w-full h-full flex justify-center items-center">
-				<input type="file" onChange={
-					(e)=>{
-
-						setFile(e.target.files[0]);
-
-					}
-				} />
-				<button onClick={handleUpload} className="bg-red-900 p-2 text-white rounded-xl">Upload</button>
+			<div className="w-full h-full bg-red-900 lg:bg-green-900">
+				
 			</div>
 
 	);
